3ea8834455a67b02687080d36fc09657fda9c181
siteng <sitengzhang2009@gmail.com>
2024-05-22 19:12:51 -0400

    Update timeline page

---
+++ src/components/collapsible.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+import { FaChevronDown } from "react-icons/fa";
+import './collapsible.css'
+
+export const Collapsible = ({date, word, image}) => {
+    const [open, setOpen] = useState(false);
+    const toggle = () => {
+        setOpen(!open);
+    }
+    
+    return (
+      <div>
+        <div className="toggle">
+        <p>{date}</p>
+        <Button className='button' variant="secondary" onClick={() => toggle()}><FaChevronDown></FaChevronDown></Button>
+            </div>
+        <p>_____________________________________________________________________________________________________________________</p>
+
+        
+        {open &&<div>{word}</div>}
+        {open&& <p></p>}
+        {open && <div><img src={image[0]} width={image[1]} height={image[2]}></img> </div>}
+      </div>
+    );
+  }
--- src/pages/timeline.tsx
+++ src/pages/timeline.tsx
@@ -1,18 +1,16 @@
-import initHome from '../assets/5_19.jpg'
-import initgames from '../assets/5_19_games.jpg'
-import tools from '../assets/5_20_tools.jpg'
-import main from '../assets/5_21_main.jpg'
+import {Collapsible} from '../components/collapsible'
 
 export const TimeLine = () => {
     return (
       <>
-        <h2>This page acts as a timeline to tract my progress!</h2>
-        <p>5/18: Created the app using vite, bun and react. Bought the domain on CloudFlare and hooked up the github integration with it.</p>
-        <p>5/19: Updated app to have multiple pages, a working (albeit ugly) navigation bar and this timeline page. </p>
-        <img src={initHome} width={250} height={50}></img> <img src={initgames} width={250} height={50}></img>
-        <p>5/20: Added tools page and the first tools factor finder. <img src={tools} width={500} height={200}></img></p>
-        <p>5/21: Revamped the main page to actually show something and added two new page that I'll eventually get to. <img src={main} width={300} height={340}></img></p>
-        <p>5/22: Desparately need to revamp the timeline page to look ok. </p>
+        <h2>This page acts as a timeline to track my progress, click on the icon to expand and see progress made that day.</h2>
+        <Collapsible date='5/18' word='Created the app using vite, bun and react. Bought the domain on CloudFlare and hooked up the github integration with it.' image={[]}> </Collapsible>
+        <Collapsible date='5/19' word='Updated app to have multiple pages, a working (albeit ugly) navigation bar and this timeline page.' image={['src/assets/5_19.jpg', 800, 160]}> </Collapsible>
+        <Collapsible date='5/20' word='Added tools page and the first tools factor finder.' image={['src/assets/5_20_tools.jpg', 800, 300]}> </Collapsible>
+        <Collapsible date='5/21' word='Revamped the main page to actually show something and added two new page that I will eventually get to.' image={['src/assets/5_21_main.jpg', 800, 920]}> </Collapsible>
+        <Collapsible date='5/22' word='Revamp timeline page to have a collapsible component.' image={['src/assets/5_22_timeline.jpg', 800, 800]}> </Collapsible>
+        <Collapsible date='5/23' word='Probably update the games page.' image={[]}> </Collapsible>
+
       </>
     );
   };
